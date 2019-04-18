ccdaf9c1660e169fee134bb402165c0604482e97
Mahdi <m.pourziaei@gmail.com>
2019-04-18 17:23:14 +0800

    [Pending test] Updated gh-pages script and travis spec

--- build/gh-pages.js
+++ build/gh-pages.js
@@ -1,10 +1,14 @@
 module.exports = function (grunt) {
+    const is_travis_cd = process.env.TRAVIS === 'true' && process.env.TRAVIS_PULL_REQUEST === 'false';
+    const origin_with_auth = `https://${process.env.GHT}@github.com/${process.env.TRAVIS_REPO_SLUG}.git`;
+
     return {
         main: {
             options: {
                 add    : (grunt.option('cleanup') ? false : true),
                 base   : 'dist',
                 branch : 'gh-pages',
+                repo   : is_travis_cd ? origin_with_auth : undefined,
                 message: global.is_release ? `Release to ${global.release_target}` : `Deploy to ${global.branch || 'gh-pages'}`,
                 ...(global.is_release && {
                     repo : global.release_info.target_repo,
